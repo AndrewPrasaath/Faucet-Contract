5c1a4f9ff617953cd37bcd053926295e74eb6301
Andrew Prasaath <andrewprasaath07@gmail.com>
2023-01-17 22:12:18 +0530

    init

---
+++ contract/Faucet.sol
@@ -0,0 +1,43 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Owned {
+    address payable immutable i_owner;
+
+    constructor() {
+        i_owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert("Only contract owner can call this function");
+            _;
+        }
+    }
+}
+
+contract Mortal is Owned {
+    function destroy() public onlyOwner {
+        selfdestruct(i_owner);
+    }
+}
+
+contract Faucet is Mortal {
+    event Withdraw(address to, uint amount);
+    event Deposit(address from, uint amount);
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint _withdrawAmount) public {
+        if (_withdrawAmount > 0.1 ether) revert();
+        if (address(this).balance < _withdrawAmount)
+            revert("Insufficient Fund");
+
+        payable(msg.sender).transfer(_withdrawAmount);
+
+        emit Withdraw(msg.sender, _withdrawAmount);
+    }
+}
---
+++ contract/Token.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./Faucet.sol";
+
+contract Token is Mortal {
+    Faucet _faucet;
+
+    constructor(address _f) {
+        _faucet = Faucet(_f);
+        _faucet.withdraw(0.1 ether);
+    }
+}
