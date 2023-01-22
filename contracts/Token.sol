80c7d82ef3964cd22451478fa50b4ca73eefae09
Andrew Prasaath <andrewprasaath07@gmail.com>
2023-01-22 17:46:18 +0530

    hardhat setup

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,7 +7,7 @@ import "./Faucet.sol";
 contract Token is Mortal {
     Faucet _faucet;
 
-    constructor(address _f) {
+    constructor(address payable _f) {
         _faucet = Faucet(_f);
         _faucet.withdraw(0.1 ether);
     }
