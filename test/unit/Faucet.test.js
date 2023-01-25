1b0665c52060389ff894f58cdd9c9af362ed80c1
Andrew Prasaath <andrewprasaath07@gmail.com>
2023-01-25 07:46:11 +0530

    add test script

--- test/unit/Faucet.test.js
+++ test/unit/Faucet.test.js
@@ -1,21 +1,29 @@
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Faucet", function () {
-  let Faucet, faucet, owner;
+  let Faucet, faucet, owner, signer1;
 
   beforeEach("Faucet", async function () {
     Faucet = await ethers.getContractFactory("Faucet");
     faucet = await Faucet.deploy();
     //await faucet.deployed();
 
-    [owner] = await ethers.getSigners();
+    [owner, signer1] = await ethers.getSigners();
   });
 
   describe("constructor", function () {
     it("should assign owner correctly", async function () {
       const faucetOwner = await faucet.getOwner();
       assert.equal(faucetOwner, owner.address);
+    });
+  });
+
+  describe("destroy", function () {
+    it("no one able to destroy the contract except owner", async function () {
+      await expect(faucet.connect(signer1).destroy()).to.be.revertedWith(
+        "Only contract owner can call this function"
+      );
     });
   });
 });
