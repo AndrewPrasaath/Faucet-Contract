79b63c35bc60e1d9835c3618b3a7197eabe52041
Andrew Prasaath <andrewprasaath07@gmail.com>
2023-01-28 08:02:27 +0530

    Added test scripts

--- test/unit/Faucet.test.js
+++ test/unit/Faucet.test.js
@@ -2,12 +2,14 @@ const { assert, expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Faucet", function () {
-  let Faucet, faucet, owner, signer1;
+  let Faucet, faucet, owner, signer1, value, withdrawAmt;
 
   beforeEach("Faucet", async function () {
     Faucet = await ethers.getContractFactory("Faucet");
     faucet = await Faucet.deploy();
     [owner, signer1] = await ethers.getSigners();
+    value = ethers.utils.parseEther("1");
+    withdrawAmt = ethers.utils.parseEther("0.1");
   });
 
   describe("constructor", function () {
@@ -34,7 +36,7 @@ describe("Faucet", function () {
     it("emits Deposit event while calling receive", async function () {
       const transaction = {
         to: faucet.address,
-        value: ethers.utils.parseEther("1"),
+        value: value,
       };
       const tx = await owner.sendTransaction(transaction);
       expect(tx).to.emit(faucet, "Deposit");
@@ -43,11 +45,43 @@ describe("Faucet", function () {
       //const beforeBalance = await faucet.getBalance();
       await owner.sendTransaction({
         to: faucet.address,
-        value: ethers.utils.parseEther("1"),
+        value: value,
       });
       const balance = await ethers.provider.getBalance(faucet.address);
 
       assert.equal(balance.toString(), "1000000000000000000");
+    });
+  });
+
+  describe("withdraw", function () {
+    it("should revert if withdraw amt is higher than 0.1 ether", async function () {
+      await expect(faucet.connect(owner).withdraw(value)).to.be.revertedWith(
+        "Exeeds withdraw limit!"
+      );
+    });
+    it("should revert if faucet balance less than withdraw amt", async function () {
+      await expect(
+        faucet.connect(owner).withdraw(withdrawAmt)
+      ).to.be.revertedWith("Insufficient Fund");
+    });
+    it("fuacet balance should update after withdraw", async function () {
+      await owner.sendTransaction({
+        to: faucet.address,
+        value: value,
+      });
+      await faucet.connect(owner).withdraw(withdrawAmt);
+      const balance = await ethers.provider.getBalance(faucet.address);
+      assert.equal(balance.toString(), value - withdrawAmt);
+    });
+    it("should emit Withdraw event", async function () {
+      await owner.sendTransaction({
+        to: faucet.address,
+        value: value,
+      });
+      expect(await faucet.connect(owner).withdraw(withdrawAmt)).to.emit(
+        faucet,
+        "Withdraw"
+      );
     });
   });
 });
